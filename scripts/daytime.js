4ea98dabaac85e7caf73c87b4c8688bba9a2e0cd
Benjamin Shu <benjamin.shu@stonybrook.edu>
2019-01-13 02:35:16 -0500

    Added support for cycling through 24 background colors and alternating between night and day.

--- scripts/daytime.js
+++ scripts/daytime.js
@@ -3,24 +3,42 @@ hourCodes = ["#003399", "#0040a5", "#004cb1", "#0059bd", "#0066cc", "#0075d2",
 	"#00e1ff", "#00ccff", "#00c0fa", "#00b1f2", "#00a2ea", "#0093e2",
 	"#0084da", "#0075d2", "#0066cc", "#0059bd", "#004cb1", "#0040a5" ];
 
+url_day = "url('css/bg_day.png')";
+url_night = "url('css/bg_night.png')";
+connect = " no-repeat cover ";
+
+count = 0;
+
 /* When page loads, start by waiting until next hour. */
 /* (i.e. if page loads at 7:53, wait until 8:00 to start looping through BG colors. */
 function start() {
-	var d = new Date(), h = d.getHours(), m = 1000 - d.getMilliseconds();
-	document.body.style.background = hourCodes[h];
-	
+	var d = new Date(), h = d.getHours(), m = (3600 * 1000) - d.getMilliseconds();
+	document.body.style.backgroundImage = ((h > 3 && h < 21) ? url_day : url_night);
+	document.body.style.backgroundPosition = "top";
+	document.body.style.backgroundRepeat = "no-repeat";
+	document.body.style.backgroundSize = "contain";
+	document.body.style.backgroundColor = hourCodes[h];
 	setTimeout(loop, m);
 }
 
 /* Function starts loop, then calls update() at hour-long intervals. */
 function loop() {
 	var h = new Date().getHours();
-	document.body.style.background = hourCodes[h];
-	setInterval(update, 3600 * 1000);
+	document.body.style.backgroundImage = ((h > 3 && h < 21) ? url_day : url_night);
+	document.body.style.backgroundPosition = "top";
+	document.body.style.backgroundRepeat = "no-repeat";
+	document.body.style.backgroundSize = "contain";
+	document.body.style.backgroundColor = hourCodes[h];
+	setInterval(update, 3 * 1000);
 }
 
 /* Function called repeatedly to transition to next color. */
 function update() {
 	var h = new Date().getHours();
-	document.body.style.background = hourCodes[h];
+	document.body.style.backgroundImage = ((count > 3 && count < 21) ? url_day : url_night);
+	document.body.style.backgroundPosition = "top";
+	document.body.style.backgroundRepeat = "no-repeat";
+	document.body.style.backgroundSize = "contain";
+	document.body.style.backgroundColor = hourCodes[count];
+	count++;
 }
